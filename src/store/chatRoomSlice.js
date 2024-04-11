a9a1bc1ab847f768aff6291c2dc71f4187c781fe
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-04-11 11:26:26 +0900

    feat: redux 설정

---
+++ src/store/chatRoomSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  currentChatRoom: {
+    createdBy: {
+      image: '',
+      name: '',
+    },
+    description: '',
+    id: '',
+    name: '',
+  },
+  isPrivateChatRoom: false,
+  userPosts: null,
+};
+
+export const chatRoomSlice = createSlice({
+  name: 'chatRoom',
+  initialState,
+  reducers: {},
+});
+
+export default chatRoomSlice.reducer;
---
+++ src/store/userSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  currentUser: {
+    uid: '',
+    photoURL: '',
+    displayName: '',
+  },
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+});
+
+export default userSlice.reducer;
