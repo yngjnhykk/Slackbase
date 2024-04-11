5c2c01c21eddbcca07162ad0ffd0d6e30fd69a28
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-04-11 11:18:10 +0900

    Feat: Vite init, ESLint, react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import ChatPage from './pages/ChatPage/ChatPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<ChatPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+    </Routes>
+  );
+}
+
+export default App;
