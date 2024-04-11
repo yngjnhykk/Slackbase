ce0ca0b75b25eab2d3f0d6ab9bbc74e52af6b1a0
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-04-11 19:26:41 +0900

    feat: firebase 회원가입

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from '../../firebase';
 
 const RegisterPage = () => {
+  const auth = getAuth(app);
+  const [loading, setLoading] = useState(false);
+  const [errorFromSubmit, setErrorFromSubmit] = useState('');
+
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      const createdUser = await createUserWithEmailAndPassword(auth, data.email, data.password);
+      console.log(createdUser);
+    } catch (err) {
+      console.error(err);
+      setErrorFromSubmit(err.message);
+      setTimeout(() => {
+        setErrorFromSubmit('');
+      }, 3000);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className='auth-wrapper'>
       <div style={{ textAlign: 'center' }}>
         <h3>Register</h3>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='email'>Email</label>
-        <input name='email' type='email' id='email' />
+        <input name='email' type='email' id='email' {...register('email', { required: true, pattern: /^\S+@\S+$/i })} />
+        {errors.email && <p>This Email field is required</p>}
 
         <label htmlFor='name'>Name</label>
-        <input type='text' name='name' id='name' />
+        <input type='text' name='name' id='name' {...register('name', { required: true, maxLength: 10 })} />
+        {errors.name && errors.name.type === 'required' && <p>This name field is required</p>}
+        {errors.name && errors.name.type === 'maxLength' && <p>Youre input exceeded maximum length</p>}
 
         <label htmlFor='password'>Password</label>
-        <input type='password' name='password' id='password' />
+        <input type='password' name='password' id='password' {...register('password', { required: true, minLength: 6 })} />
+        {errors.password && errors.password.type === 'required' && <p>This password field is required</p>}
+        {errors.password && errors.password.type === 'minLength' && <p>Password must have at least 6 characters</p>}
 
-        <input type='submit' disabled />
+        {errorFromSubmit && <p>{errorFromSubmit}</p>}
+
+        <input type='submit' disabled={loading} />
         <Link style={{ color: 'gray', textDecoration: 'none' }} to={'/login'}>
           이미 아이디가 있다면...
         </Link>
