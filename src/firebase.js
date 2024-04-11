73bd7728e679d026dbb8e19f17d2afdfd779e6ab
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-04-11 15:17:28 +0900

    feat: Firebase 연결

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOrnDJlnai4qKmnYiL4m7GUz4iMLXDq9U',
+  authDomain: 'slackbase-fcfc5.firebaseapp.com',
+  projectId: 'slackbase-fcfc5',
+  storageBucket: 'slackbase-fcfc5.appspot.com',
+  messagingSenderId: '384683849301',
+  appId: '1:384683849301:web:ec3360109d87a6941f06dc',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
