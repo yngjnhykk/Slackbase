ae340f81f4456b734d144e879e06773098fa8a5f
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-04-12 11:20:14 +0900

    feat: LoginPage 100%

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import app from '../../firebase';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const auth = getAuth(app);
+  const [loading, setLoading] = useState(false);
+  const [errorFromSubmit, setErrorFromSubmit] = useState('');
+
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+    } catch (err) {
+      console.error(err);
+      setErrorFromSubmit(err.message);
+      setTimeout(() => {
+        setErrorFromSubmit('');
+      }, 3000);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className='auth-wrapper'>
+      <div style={{ textAlign: 'center' }}>
+        <h3>Login</h3>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='email'>Email</label>
+        <input name='email' type='email' id='email' {...register('email', { required: true, pattern: /^\S+@\S+$/i })} />
+        {errors.email && <p>This Email field is required</p>}
+
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' {...register('password', { required: true, minLength: 6 })} />
+        {errors.password && errors.password.type === 'required' && <p>This password field is required</p>}
+        {errors.password && errors.password.type === 'minLength' && <p>Password must have at least 6 characters</p>}
+
+        {errorFromSubmit && <p>{errorFromSubmit}</p>}
+
+        <input type='submit' disabled={loading} />
+        <Link style={{ color: 'gray', textDecoration: 'none' }} to={'/register'}>
+          아직 아이디가 없다면
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
