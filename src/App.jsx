c75702a145a8f0756d01e48b4a89603a99aa7f7d
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-04-12 12:17:27 +0900

    feat: Redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+
 import './App.css';
 import ChatPage from './pages/ChatPage/ChatPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import app from './firebase';
 
 function App() {
+  const auth = getAuth(app);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate('/');
+      } else {
+        navigate('/login');
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<ChatPage />} />
